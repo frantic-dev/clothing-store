197f189342ad3a09e3d1f40ea70e179ecce49102
frantic-dev <okeyamazing@gmail.com>
2024-04-21 00:24:23 +0100

    added heart and cart icons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,27 @@
 import '../styles/components/navbar.scss'
+import HeartIcon from '../assets/heart-icon.svg?react'
+import CartIcon from '../assets/cart-icon.svg?react'
 
 function Navbar() {
   return (
     <nav>
       <div>Logo</div>
       <div id='nav-links'>
-        <a href="">Home</a>
-        <a href="">Shop</a>
-        <a href="">Our Story</a>
-        <a href="">Blog</a>
-        <a href="">Contact Us</a>
+        <a href=''>Home</a>
+        <a href=''>Shop</a>
+        <a href=''>Our Story</a>
+        <a href=''>Blog</a>
+        <a href=''>Contact Us</a>
       </div>
       <div>
-        <a href="">heart</a>
-        <a href="">cart</a>
+        <a href=''>
+          <HeartIcon className='icon' />
+        </a>
+        <a href=''>
+          <CartIcon className='icon' />
+        </a>
         <button>Login</button>
       </div>
-
-
     </nav>
   )
 }

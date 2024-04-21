e9805cb8fa18532b7e1d8b47dd2b7b1c43ce7264
frantic-dev <okeyamazing@gmail.com>
2024-04-21 19:17:27 +0100

    product card buttons displayed when mouseover card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,29 @@
 import WhiteButton from './WhiteButton'
 import '../styles/components/product-card.scss'
+import HeartIcon from '../assets/heart-icon.svg?react'
+import { useState } from 'react'
+
 function ProductCard(props) {
+  const [hideElements, setHideElements] = useState(true)
+
   return (
-    <div className='product-card'>
+    <div
+      className='product-card'
+      onMouseOver={() => setHideElements(false)}
+      onMouseOut={() => setHideElements(true)}
+    >
       <div>
         <img src={props.img} />
-        <WhiteButton text='Add to Cart' />
+        <WhiteButton
+          text='Add to Cart'
+          hide={hideElements}
+        />
+        <div
+          className='heart-icon'
+          style={{ display: hideElements ? 'none' : 'flex' }}
+        >
+          <HeartIcon />
+        </div>
       </div>
       <h3>{props.title}</h3>
       <div className='description'>{props.description}</div>
--- src/components/WhiteButton.jsx
+++ src/components/WhiteButton.jsx
@@ -1,8 +1,11 @@
 import '../styles/components/white-btn.scss'
 
-function WhiteButton(props){
-  return(
-    <button className="white-btn">
+function WhiteButton(props) {
+  return (
+    <button
+      className='white-btn'
+      style={{ display: props.hide ? 'none' : '' }}
+    >
       {props.text}
     </button>
   )

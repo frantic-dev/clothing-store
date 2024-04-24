5163e8307c025fcc850800a9ac6f9aca3e7a16e9
frantic-dev <okeyamazing@gmail.com>
2024-04-24 01:05:59 +0100

    created sqlalchemy db with user model

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models

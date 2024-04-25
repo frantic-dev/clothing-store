dd9e5db02d33d56745ef3ad4c3c8f4947737a49b
frantic-dev <okeyamazing@gmail.com>
2024-04-25 23:38:34 +0100

    handle signup form state change

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,49 +1,79 @@
+import { useState } from 'react'
+
 function SignupForm() {
+  const [signupData, setSignupData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    agreeToTerms: false,
+  })
+
+  function handleChange(e) {
+    setSignupData({ ...signupData, [e.target.name]: e.target.value })
+  }
+
+  function toggleAgreement() {
+    setSignupData(data => ({ ...data, agreeToTerms: !data.agreeToTerms }))
+  }
+
   return (
     <div>
-      <h2 className="form-title">create new account</h2>
-      <div className="form-instructions">Please enter details</div>
+      <h2 className='form-title'>create new account</h2>
+      <div className='form-instructions'>Please enter details</div>
 
-      <form className="form">
+      <form className='form'>
         <label htmlFor='first-name'>email address</label>
         <input
           type='text'
-          name='first-name'
+          name='firstName'
           id='first-name'
-          placeholder="Jane"
+          placeholder='Jane'
+          value={signupData.firstName}
+          onChange={handleChange}
         />
 
         <label htmlFor='last-name'>last name</label>
         <input
-          type='password'
-          name='last-name'
+          type='text'
+          name='lastName'
           id='last-name'
-          placeholder="Doe"
+          placeholder='Doe'
+          value={signupData.lastName}
+          onChange={handleChange}
         />
 
         <label htmlFor='signup-email'>email address</label>
         <input
           type='text'
-          name='signup-email'
+          name='email'
           id='signup-email'
-          placeholder="example@gmail.com"
+          placeholder='example@gmail.com'
+          value={signupData.email}
+          onChange={handleChange}
         />
 
         <label htmlFor='signup-password'>password</label>
         <input
           type='password'
-          name='signup-password'
+          name='password'
           id='signup-password'
-          placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+          placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;'
+          value={signupData.password}
+          onChange={handleChange}
         />
 
         <div>
           <label>
-            <input type='checkbox' /> i agree to the terms & conditions
+            <input
+              type='checkbox'
+              onClick={toggleAgreement}
+            />{' '}
+            i agree to the terms & conditions
           </label>
         </div>
 
-        <button className="form-btn">Signup</button>
+        <button className='form-btn'>Signup</button>
       </form>
     </div>
   )
